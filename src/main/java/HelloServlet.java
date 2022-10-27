0795b015e89289ae867acbced8e71aa7c3523701
st3aks <ajr007@marietta.edu>
2022-10-26 23:45:29 -0400

    tony branch inaugural push

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -34,7 +34,7 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().println("Hello");
+		response.getWriter().println("Hello Tony");
 	}
 
 	/**
