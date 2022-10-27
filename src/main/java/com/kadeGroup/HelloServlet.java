3cf4cd565f5100b8a83ec8b903b2c55d292e048a
KadeHennacy <kade.hennacy@gmail.com>
2022-10-27 15:25:46 -0400

    Messed around with redirect, forwarding, sessions

--- src/main/java/com/kadeGroup/HelloServlet.java
+++ src/main/java/com/kadeGroup/HelloServlet.java
@@ -1,3 +1,4 @@
+package com.kadeGroup;
 
 import java.io.IOException;
 import java.io.PrintWriter;
---
+++ src/main/java/com/kadeGroup/SquareServlet.java
@@ -0,0 +1,22 @@
+package com.kadeGroup;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SquareServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		Passing values with session
+//		HttpSession session = req.getSession();
+//		int k = (int) session.getAttribute("k");
+		PrintWriter out = res.getWriter();
+		out.println(k + "^2 = " + k * k);
+	}
+
+}
