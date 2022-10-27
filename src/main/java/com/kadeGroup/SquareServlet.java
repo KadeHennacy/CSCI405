ff7b16752d522bc3e958d5f06c01ceab5c685eee
KadeHennacy <kade.hennacy@gmail.com>
2022-10-27 15:29:46 -0400

    fixeddd

--- src/main/java/com/kadeGroup/SquareServlet.java
+++ src/main/java/com/kadeGroup/SquareServlet.java
@@ -13,7 +13,7 @@ public class SquareServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 //		Passing values with session
-//		HttpSession session = req.getSession();
+		HttpSession session = req.getSession();
 		int k = (int) session.getAttribute("k");
 		PrintWriter out = res.getWriter();
 		out.println(k + "^2 = " + k * k);
