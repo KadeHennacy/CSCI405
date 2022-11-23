21b16d0392b4ae7dee284c1ba7f23ed3edc2c52b
KadeHennacy <kade.hennacy@gmail.com>
2022-11-23 13:45:49 -0500

    sql taglib

--- src/main/java/com/kadeGroup/DemoServlet.java
+++ src/main/java/com/kadeGroup/DemoServlet.java
@@ -10,14 +10,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/DemoServlet")
 public class DemoServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String name = "kade";
-		Student s = new Student(1, name);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		Student[] students = {
+				new Student(1,"Kade"),
+				new Student(2,"Bob"),
+				new Student(3,"Joe"),
+				new Student(4,"Jane"),
+				new Student(5,"Nigward")
+		};
 		RequestDispatcher rd = request.getRequestDispatcher("demo.jsp");
-		request.setAttribute("student", s);
-		rd.forward(request, response);
-			
 		
+		request.setAttribute("students", students);
+		
+		rd.forward(request, response);	
 	}
 
 }
