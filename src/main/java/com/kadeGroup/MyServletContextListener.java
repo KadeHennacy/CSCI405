e653197a019d96b283a3e05dca37087cb68db75b
KadeHennacy <kade.hennacy@gmail.com>
2022-11-30 10:21:22 -0500

    Create and delete schema when the app runs/stops

--- src/main/java/com/kadeGroup/MyServletContextListener.java
+++ src/main/java/com/kadeGroup/MyServletContextListener.java
@@ -10,16 +10,13 @@ public class MyServletContextListener implements ServletContextListener {
 
 		  @Override
 		  public void contextDestroyed(ServletContextEvent arg0) {
-			  System.out.println("Shutting down");
-		    //Notification that the servlet context is about to be shut down.   
+			  LoginDao dao = new LoginDao();
+			  dao.dropAccountTable();
 		  }
 
 		  @Override
 		  public void contextInitialized(ServletContextEvent arg0) {
-		    // do all the tasks that you need to perform just after the server starts
-
-		    //Notification that the web application initialization process is starting
-			  
-			  System.out.println("Starting up");
+			  LoginDao dao = new LoginDao();
+			  dao.createAccountTable();
 		  }
 }
